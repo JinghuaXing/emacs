2a7053c6ff74765d76582e1ed90e3705b0c0cbc4
wei.sun <wei.sun@spreadtrum.com>
2012-03-10 07:35:51 -0800

    nil

--- dotemacs/my.el
+++ dotemacs/my.el
@@ -438,5 +438,6 @@ occurence of CHAR."
   (open-line arg)
   (next-line 1)
     (indent-according-to-mode))
-(global-set-key (kbd "C-o") 'open-next-line)
+(global-set-key (kbd "M-o") 'open-next-line)
+(global-unset-key (kbd "C-o"))
 ;; Behave like vi's O command
