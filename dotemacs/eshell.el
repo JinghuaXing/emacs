b85986a8fe84cc6c32a63514a6f72ae62e34338e
wei.sun <wei.sun@spreadtrum.com>
2012-03-19 12:44:40 +0800

    nil

--- dotemacs/eshell.el
+++ dotemacs/eshell.el
@@ -9,6 +9,16 @@
 	  '(lambda ()
 	     (define-key eshell-mode-map "\C-l" 'my-eshell-clear-buffer)
 	     (define-key eshell-mode-map "\C-u" 'eshell-kill-input)
+	     (define-key eshell-mode-map [return] 'user-ret)
+	     (define-key eshell-mode-map (kbd "SPC") 'user-spc)
+	     (outline-minor-mode 1)
+	     (setq outline-regexp "^[^#$\n]* [#>]+ "
+		   scroll-margin 0
+		   eshell-scroll-to-bottom-on-output t
+		   eshell-scroll-show-maximum-output t)
+	     (add-to-list 'eshell-output-filter-functions 
+			  'eshell-postoutput-scroll-to-bottom)
+
 	     ))
 
 ;;(setq eshell-cp-interactive-query t
@@ -118,3 +128,68 @@
 
 ;; alias
 (defalias 'ff 'find-file)
+
+
+;;
+;; * tab
+(defun user-tab ()
+  (interactive)
+  (let ((input (eshell-get-old-input)))
+    (if (string-equal input "")
+        (insert-string "cd ")
+      (progn
+        (cond
+         ((string-equal input "cd  ")
+          (delete-backward-char 1)
+          (insert-string "-0"))
+         ((string-match "^cd\\ -[0-9]+$" input)
+          (let ((len (length input)))
+            (delete-backward-char (- len 4))
+            (insert-string (1+ (read(substring input 4 len))))))
+         (t
+          (pcomplete))
+         )))))
+
+;; * ret
+(defun user-ret ()
+  (interactive)
+  (let ((input (eshell-get-old-input)))
+    (if (string-equal input "")
+        (progn
+          (insert-string "ls")
+          (eshell-send-input))
+      (progn
+        (cond
+         ((string-match "^cd\\ \\.\\{2,\\}$" input)
+          (let ((len (- (length input) 5))
+                (dots (lambda (n d)(if (<= n 0) nil (concat (funcall dots (1- n) d) d)))))
+            (eshell-bol)(kill-line)
+            (insert-string (concat "cd .." (funcall dots len "/..")))
+            (eshell-send-input)))
+         ((string-match "^\\.\\{2,\\}$" input)
+          (let ((len (- (length input) 2))(p ".."))
+            (while (> len 0)
+              (setq len (1- len))(setq p (concat p "/..")))
+            (eshell-bol)(kill-line)
+            (insert-string p)
+            (eshell-send-input)))
+         (t
+          (eshell-send-input))))
+      )))
+
+;; * spc
+(defun user-spc ()
+  (interactive)
+  (let ((input (eshell-get-old-input)))
+    (if (string-equal input "")
+        (insert-string "!-1")
+      (progn
+        (cond
+         ((string-match "^!-[0-9]+$" input)
+          (let ((len (length input)))
+            (delete-backward-char (- len 2))
+            (insert-string (1+ (read(substring input 2 len))))))
+         (t
+          (self-insert-command 1))
+         )))))
+
