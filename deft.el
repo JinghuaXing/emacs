fc8261f7994e33bc519bf4426a6f76a511369a14
wei.sun <wei.sun@spreadtrum.com>
2013-05-27 11:48:11 +0800

    add support for directory-files-recursive for deft

--- deft.el
+++ deft.el
@@ -504,14 +504,52 @@ is the complete regexp."
       (setq file (replace-regexp-in-string (concat "\." deft-extension "$") "" file)))
   file)
 
+(defun directory-files-recursive (directory)
+  "List the .el files in DIRECTORY and in its sub-directories."
+  ;; Although the function will be used non-interactively,
+  ;; it will be easier to test if we make it interactive.
+  ;; The directory will have a name such as
+  ;;  "/usr/local/share/emacs/22.1.1/lisp/"
+  (interactive "DDirectory name: ")
+  (let (el-files-list
+	(current-directory-list
+	 (directory-files-and-attributes directory t)))
+    ;; while we are in the current directory
+    (while current-directory-list
+      (cond
+       ;; check to see whether filename ends in `.el'
+       ;; and if so, append its name to a list.
+       ((equal "org" (substring (car (car current-directory-list)) -3))
+	(setq el-files-list
+	      (cons (car (car current-directory-list)) el-files-list)))
+       ;; check whether filename is that of a directory
+       ((eq t (car (cdr (car current-directory-list))))
+	;; decide whether to skip or recurse
+	(if
+	    (equal "."
+		   (substring (car (car current-directory-list)) -1))
+	    ;; then do nothing since filename is that of
+	    ;;   current directory or parent, "." or ".."
+	    ()
+	  ;; else descend into the directory and repeat the process
+	  (setq el-files-list
+		(append
+		 (directory-files-recursive
+		  (car (car current-directory-list)))
+		 el-files-list)))))
+      ;; move to the next filename in the list; this also
+      ;; shortens the list so the while loop eventually comes to an end
+      (setq current-directory-list (cdr current-directory-list)))
+    ;; return the filenames
+    el-files-list))
+
 (defun deft-find-all-files ()
   "Return a list of all files in the Deft directory."
   (if (file-exists-p deft-directory)
       (let (files result)
         ;; List all files
         (setq files
-              (directory-files deft-directory t
-                               (concat "\." deft-extension "$") t))
+              (directory-files-recursive deft-directory))
         ;; Filter out files that are not readable or are directories
         (dolist (file files)
           (when (and (file-readable-p file)
