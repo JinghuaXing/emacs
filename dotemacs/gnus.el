1f33525ae8611b338b8ac6dc27eca654183c5a09
wei.sun <wei.sun@spreadtrum.com>
2014-01-22 10:47:58 +0800

    nil

--- dotemacs/gnus.el
+++ dotemacs/gnus.el
@@ -96,14 +96,14 @@ Email:wei.sun@spreadtrum.com
 (setq gnus-message-archive-group
       '("nnimap+spreadtrum:Sent"))
 
-(setq nnmail-expiry-wait-function
-      (lambda (group)
-	(cond ((string= group "todo")
-	       1)
-	      ((string= group "archives")
-	       1)
-	      (t
-	       1))))
+;; (setq nnmail-expiry-wait-function
+;;       (lambda (group)
+;; 	(cond ((string= group "todo")
+;; 	       1)
+;; 	      ((string= group "archives")
+;; 	       1)
+;; 	      (t
+;; 	       1))))
 
 (setq nnmail-expiry-target "nnimap+spreadtrum:Trash")
 
@@ -161,3 +161,27 @@ Email:wei.sun@spreadtrum.com
 
 (define-key gnus-group-mode-map (kbd "q") 'gnus-group-suspend)
 (define-key gnus-summary-mode-map (kbd "S-SPC") 'gnus-summary-prev-page)
+(define-key gnus-summary-mode-map (kbd "<delete>") 'gnus-summary-delete-article)
+
+(copy-face 'font-lock-variable-name-face 'gnus-face-6)
+(setq gnus-face-6 'gnus-face-6)
+(copy-face 'font-lock-constant-face 'gnus-face-7)
+(setq gnus-face-7 'gnus-face-7)
+(copy-face 'gnus-face-7 'gnus-summary-normal-unread)
+(copy-face 'font-lock-constant-face 'gnus-face-8)
+(set-face-foreground 'gnus-face-8 "gray50")
+(setq gnus-face-8 'gnus-face-8)
+(copy-face 'font-lock-constant-face 'gnus-face-9)
+(set-face-foreground 'gnus-face-9 "gray70")
+(setq gnus-face-9 'gnus-face-9)
+(setq gnus-summary-make-false-root 'dummy)
+;; (setq gnus-summary-make-false-root-always nil)
+(setq gnus-summary-dummy-line-format "    %8{│%}   %(%8{│%}                       %7{│%}%) %6{□%}  %S\n"
+	gnus-summary-line-format "%8{%4k│%}%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{│%} %6{%B%} %s\n"
+	gnus-sum-thread-tree-indent " "
+	gnus-sum-thread-tree-root "■ "
+	gnus-sum-thread-tree-false-root "□ "
+	gnus-sum-thread-tree-single-indent "▣ "
+	gnus-sum-thread-tree-leaf-with-other "├─▶ "
+	gnus-sum-thread-tree-vertical "│"
+	gnus-sum-thread-tree-single-leaf "└─▶ ")
